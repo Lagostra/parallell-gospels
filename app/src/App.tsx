bd072c0d029df8006f48e3bbd37da58eb5f018e7
Eivind Lie Andreassen <eivind.andreassen@bekk.no>
2022-05-15 20:30:36 +0200

    Add react-jss

--- app/src/App.tsx
+++ app/src/App.tsx
@@ -1,5 +1,23 @@
 import { Parallells } from "components/Parallells";
+import { createUseStyles } from "react-jss";
+
+const useStyles = createUseStyles({
+  wrapper: {},
+  main: {
+    maxWidth: "1200px",
+    width: "auto",
+    margin: "0 auto",
+  },
+});
 
 export const App = () => {
-  return <Parallells />;
+  const classes = useStyles();
+
+  return (
+    <div className={classes.wrapper}>
+      <main className={classes.main}>
+        <Parallells />
+      </main>
+    </div>
+  );
 };
--- app/src/components/Episode.tsx
+++ app/src/components/Episode.tsx
@@ -1,31 +1,43 @@
+import { createUseStyles } from "react-jss";
 import { IEpisode } from "types";
+
+const useStyles = createUseStyles({
+  gospels: {
+    display: "grid",
+    gridTemplateColumns: "repeat(auto-fit, minmax(300px, 1fr))",
+  },
+});
 
 interface IProps {
   episode: IEpisode;
 }
 export const Episode = ({ episode }: IProps) => {
+  const classes = useStyles();
+
   return (
     <div>
       <h3>{episode.title}</h3>
-      <div>
-        <h4>Matteus</h4>
-        <p>{episode.matthew}</p>
-        <p>I riktig rekkefølge: {episode.matthew_ordered ? "Ja" : "Nei"}</p>
-      </div>
-      <div>
-        <h4>Markus</h4>
-        <p>{episode.mark}</p>
-        <p>I riktig rekkefølge: {episode.mark_ordered ? "Ja" : "Nei"}</p>
-      </div>
-      <div>
-        <h4>Lukas</h4>
-        <p>{episode.luke}</p>
-        <p>I riktig rekkefølge: {episode.luke_ordered ? "Ja" : "Nei"}</p>
-      </div>
-      <div>
-        <h4>Johannes</h4>
-        <p>{episode.john}</p>
-        <p>I riktig rekkefølge: {episode.john_ordered ? "Ja" : "Nei"}</p>
+      <div className={classes.gospels}>
+        <div>
+          <h4>Matteus</h4>
+          <p>{episode.matthew}</p>
+          <p>I riktig rekkefølge: {episode.matthew_ordered ? "Ja" : "Nei"}</p>
+        </div>
+        <div>
+          <h4>Markus</h4>
+          <p>{episode.mark}</p>
+          <p>I riktig rekkefølge: {episode.mark_ordered ? "Ja" : "Nei"}</p>
+        </div>
+        <div>
+          <h4>Lukas</h4>
+          <p>{episode.luke}</p>
+          <p>I riktig rekkefølge: {episode.luke_ordered ? "Ja" : "Nei"}</p>
+        </div>
+        <div>
+          <h4>Johannes</h4>
+          <p>{episode.john}</p>
+          <p>I riktig rekkefølge: {episode.john_ordered ? "Ja" : "Nei"}</p>
+        </div>
       </div>
     </div>
   );
